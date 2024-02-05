15993059339cbcc47ad47eab6a295bb029d7fb29
unknown <alitarek868@yahoo.com>
2024-02-04 17:11:16 -0800

    responsive

--- js/script.js
+++ js/script.js
@@ -30,27 +30,51 @@ linksContainer.addEventListener('click',function(e) {
 
 
 
-const stickyNav= function(entries, observer) {
-    const [entry]= entries;
-    if(!entry.isIntersecting) 
-    header.classList.add('nav-fixed');
+// const stickyNav= function(entries, observer) {
+//     const [entry]= entries;
+//     if(!entry.isIntersecting) 
+//     header.classList.add('nav-fixed');
 
-    else  
-    header.classList.remove('nav-fixed');
+//     else  
+//     header.classList.remove('nav-fixed');
 
-}
-
-
-const navObserver= new IntersectionObserver(stickyNav, {
-    root: null,
-    threshold: 0,
-    rootMargin: '100px'
-
-});
+// }
 
 
-navObserver.observe(sectionHow)
+// const navObserver= new IntersectionObserver(stickyNav, {
+//     root: null,
+//     threshold: 0,
+//     rootMargin: '100px'
+
+// });
 
 
+// navObserver.observe(sectionHow)
+
+
+
+const navIconContainer= document.querySelector('.btn-mobile-nav');
+const navIcon= document.querySelectorAll('.icon-mobile-nav')
+
+
+navIconContainer.addEventListener('click', function(e) {
+    console.log(e.target.getAttribute('name'), "Event");
+
+
+    navIcon.forEach((icon)=> {
+       icon.style.display='block'
+    });
+
+    e.target.style.display= 'none';
+
+
+    if(e.target.getAttribute('name') == 'menu-outline') {
+      document.querySelector('header').classList.add('nav-open');
+    }else {
+        document.querySelector('header').classList.remove('nav-open')
+
+    }
+
+})
 
 
